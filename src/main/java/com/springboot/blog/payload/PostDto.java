3777588a5ce23fa93615bc34fb9cced0d2f2a1c2
OmarHamdi11 <omarellafy1@gmail.com>
2025-10-19 16:42:59 +0300

    enable validation on create and update post

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -1,6 +1,9 @@
 package com.springboot.blog.payload;
 
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +14,19 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostDto {
+
     private long id;
+
+    @NotEmpty(message = "post title must be not empty")
+    @Size(min = 2,message = "post title must be more than 2 characters")
     private String title;
+
+    @NotEmpty(message = "post description must be not empty")
+    @Size(min = 10, message = "post description should be more than 10 chars")
     private String description;
+
+    @NotEmpty(message = "post content must be not empty")
     private String content;
+
     private Set<CommentDto> comments;
 }
