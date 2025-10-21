d423c7bdd1b31553c9b38db93062f5fc9b4c240a
OmarHamdi11 <omarellafy1@gmail.com>
2025-10-21 15:42:29 +0300

    create User, Role entities and there repositories

---
+++ src/main/java/com/springboot/blog/entity/Role.java
@@ -0,0 +1,23 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
